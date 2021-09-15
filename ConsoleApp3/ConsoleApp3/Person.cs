76ce6de6c3fc3d504d246f6ae04cefa88c21ae99
Kristaps230 <90153701+Kristaps230@users.noreply.github.com>
2021-09-15 21:14:35 +0300

    izmainas2

---
+++ ConsoleApp3/ConsoleApp3/Person.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Person
+
+    {
+        public Person(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+
+
+        private int _age;
+        public int Age
+        {
+            get
+            {
+                return _age;
+
+            }
+            set
+            {
+
+                _age = Math.Abs(value);
+            }
+
+        }
+
+        
+
+
+
+        
+       
+
+
+        
+
+        public double Money { get; set; }
+
+
+        public bool IsMale { get; set; }
+
+
+        public char Initial { get; set; }
+
+        public string GetGreeting()
+        {
+            string greeting = "Hello my name is " + Name + "and I am" + Age + " years old";
+
+            return greeting;
+
+        }
+
+
+
+       
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Kristaps");
+            Person person2 = person;
+            
+
+            person.Name = "Kristaps";
+            person.Age = 15;
+            person.IsMale = true;
+            person.Money = 154.58;
+            person.Initial = 'A';
+
+            person2.Age = 26;
+
+
+            string greeting = person.GetGreeting();
+
+            Console.WriteLine(greeting);
+
+
+        }
+    }
+}
